4b9d8f2a55521241382ce417d80fc12728861a43
akryvtsun <kontiky@gmail.com>
2020-05-10 19:21:01 +0300

    add TimeString test

---
+++ src/test/java/com.akryvtsun.timerecorder.controllers/TimeStringTest.java
@@ -0,0 +1,23 @@
+package com.akryvtsun.timerecorder.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeStringTest {
+
+    @Test
+    public void correctZeroInterval() {
+        assertEquals(" 0:00:00", new TimeString(0).get());
+    }
+
+    @Test
+    public void correctOneSecInterval() {
+        assertEquals(" 0:00:01", new TimeString(1000L).get());
+    }
+
+    @Test
+    public void correctOneMinuteInterval() {
+        assertEquals(" 0:01:00", new TimeString(60*1000L).get());
+    }
+}
